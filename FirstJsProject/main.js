2245054fa72d0d71686af947e62ec4aae913040d
muhammed80 <mufaah@windowslive.com>
2022-01-21 22:38:10 +0300

    FirstJsProject
    Transferring data from input to array and writing to the screen

---
+++ FirstJsProject/main.js
@@ -0,0 +1,33 @@
+window.onload = () =>{
+
+  var numbrs = [];
+
+  document.getElementById('send').addEventListener('click', ()=> {
+  var x = document.getElementById('nmbr').value;
+  
+    if (!x) {
+
+      alert("empty");
+    }else{
+
+      var html = '';
+      document.getElementById('nmbr').value = '';
+      numbrs.push(x);
+
+     for (let i = 0; i < numbrs.length; i++) {
+        
+         html += `<li>( ${numbrs[i]} )</li>`;
+     }
+     document.getElementById('h3').innerHTML = html;
+
+     var bigNumber = Math.max(...numbrs);
+     var smNumber = Math.min(...numbrs);
+
+     document.getElementById('spanB').innerHTML = "Girilen en Büyük Sayı : " + `<b style="color:green">${ bigNumber}</b>`;
+     document.getElementById('spanS').innerHTML = "Girilen en Küçük sayı : "  +`<b style="color:red">${ smNumber}</b>`;
+
+
+    }
+});
+
+}
